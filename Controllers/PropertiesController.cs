e4e970ab453c558d937a342472736b8377f022a1
Lucas Weier <wlucas7474@yahoo.com>
2020-07-08 09:27:16 -0400

    Moved ProfilesController to correct folder. Generated PropertiesController

---
+++ Controllers/PropertiesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RealEstateWebApp.Data;
+using RealEstateWebApp.Models;
+
+namespace RealEstateWebApp.Controllers
+{
+    public class PropertiesController : Controller
+    {
+        private readonly RealEstateContext _context;
+
+        public PropertiesController(RealEstateContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Properties
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Properties.ToListAsync());
+        }
+
+        // GET: Properties/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @property = await _context.Properties
+                .FirstOrDefaultAsync(m => m.PropertyId == id);
+            if (@property == null)
+            {
+                return NotFound();
+            }
+
+            return View(@property);
+        }
+
+        // GET: Properties/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Properties/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PropertyId,Address,NumBeds,NumBaths,IsStudio,Sqft,HomeType,NumParkingSpots,NumGarageSpots,AttachedGarage,LotSize,YearBuilt,LatestRenovationDate,LatestRenovationDescription,Heating,Cooling,Flooring,NumStories,HasBasement,Gas,Electric,Sewer,Internet,TV,OtherDetails")] Property @property)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@property);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@property);
+        }
+
+        // GET: Properties/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @property = await _context.Properties.FindAsync(id);
+            if (@property == null)
+            {
+                return NotFound();
+            }
+            return View(@property);
+        }
+
+        // POST: Properties/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PropertyId,Address,NumBeds,NumBaths,IsStudio,Sqft,HomeType,NumParkingSpots,NumGarageSpots,AttachedGarage,LotSize,YearBuilt,LatestRenovationDate,LatestRenovationDescription,Heating,Cooling,Flooring,NumStories,HasBasement,Gas,Electric,Sewer,Internet,TV,OtherDetails")] Property @property)
+        {
+            if (id != @property.PropertyId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@property);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PropertyExists(@property.PropertyId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@property);
+        }
+
+        // GET: Properties/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @property = await _context.Properties
+                .FirstOrDefaultAsync(m => m.PropertyId == id);
+            if (@property == null)
+            {
+                return NotFound();
+            }
+
+            return View(@property);
+        }
+
+        // POST: Properties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @property = await _context.Properties.FindAsync(id);
+            _context.Properties.Remove(@property);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PropertyExists(int id)
+        {
+            return _context.Properties.Any(e => e.PropertyId == id);
+        }
+    }
+}
